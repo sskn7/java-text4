755f692d403cf4d89b45c3c997827c795f6d8998
kana sasazawa <kana.yellow7@gmail.com>
2024-11-22 22:50:22 +0900

    hennsuu01

---
+++ src/hennsuu.java
@@ -0,0 +1,15 @@
+
+public class hennsuu {
+
+	public static void main(String[] args) {
+		
+		int suugaku = 45;
+		int eigo = 82;
+		//数学または英語が50点を超える場合、「この人は合格です」を出力する
+		if (suugaku>50||eigo>50) {
+			System.out.println("この人は合格です");
+		}
+
+	}
+
+}
