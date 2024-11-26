0d3dd7110fa043d7cd001ca996f5d5ccba6085f7
kana sasazawa <kana.yellow7@gmail.com>
2024-11-26 22:05:48 +0900

    代入の練習

---
+++ src/dainyuu.java
@@ -0,0 +1,24 @@
+
+public class dainyuu {
+
+	public static void main(String[] args) {
+		String str1 = "鈴木";
+		System.out.println(str1);
+		
+		String str2 = "Tanaka";
+		System.out.println(str2);
+		
+		String tel = "090-1234-5678";
+		System.out.println(tel);
+		
+		char char1 = 'A';
+		System.out.println(char1);
+		
+		int num1 = 12345;
+		System.out.println(num1);
+		
+		boolean boo = true;
+		System.out.println(boo);
+	}
+
+}
