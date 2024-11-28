d5f354f2e8f8dc441656467bfc75283627def6cd
kana sasazawa <kana.yellow7@gmail.com>
2024-11-28 22:21:44 +0900

    代入の練習２

--- src/dainyuu.java
+++ src/dainyuu.java
@@ -19,6 +19,22 @@ public class dainyuu {
 		
 		boolean boo = true;
 		System.out.println(boo);
+		
+		//計算式、数字を代入するときは”、’どちらも不要
+		int a = 3+3;
+		System.out.println(a);
+		
+		//””を入れると文字列のになる
+		String a2 = "3+3";
+		System.out.println(a2);
+		
+		//文字型charを代入するとき’’シングルクォーテーションで囲う
+		char aa = 'A';
+		System.out.println(aa);
+		
+		//代入の連結
+		String abc ="山田";
+		System.out.println(abc + "さん");
 	}
 
 }
